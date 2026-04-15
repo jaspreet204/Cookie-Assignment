fe854dd4d671999f6cf1a46e26ec3a29d08e4a88
jaspreet <jasspreet4640@gmail.com>
2026-04-15 11:58:27 -0500

    set cookies

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,3 +12,19 @@ const widthCheck = document.getElementById('widthCheck');
 const heightCheck = document.getElementById('heightCheck');
 
 const LIFETIME = 20;
+
+function setCookie(name, value, maxAge) {
+    let cookieString = `${encodeURIComponent(name)}=${encodeURIComponent(value)}`;
+    const options = {
+        path: '/',
+        SameSite: 'Lax'
+    };
+    for (let option in options) {
+        cookieString += `; ${option}=${options[option]}`;
+    }
+    if (maxAge) {
+        cookieString += `; max-age=${maxAge}`;
+    }
+    document.cookie = cookieString;
+}
+
