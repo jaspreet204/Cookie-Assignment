695104ad91a576616dc59c17a6899cb3b62567c5
jaspreet <jasspreet4640@gmail.com>
2026-04-15 11:54:01 -0500

    get elements and checkbox in js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,14 @@
 'use strict';
+const cookieBox = document.getElementById('cookieBox');
+const settings = document.getElementById('settings');
+//Buttons
+const acceptBtn = document.getElementById('acceptBtn');
+const settingBtn = document.getElementById('settingsBtn');
+const saveBtn = document.getElementById('saveBtn');
+//options
+const browserCheck = document.getElementById('BrowserCheck');
+const osCheck = document.getElementById('osCheck');
+const widthCheck = document.getElementById('widthCheck');
+const heightCheck = document.getElementById('heightCheck');
+
+const LIFETIME = 20;
